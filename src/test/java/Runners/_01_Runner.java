31c35a379d6f2c4a82a729c566e28d30d583d4fa
TunahanBoyaci <tunahanboyaci1@gmail.com>
2023-10-27 01:35:22 -0400

    Element Matching Automation

--- src/test/java/Runners/_01_Runner.java
+++ src/test/java/Runners/_01_Runner.java
@@ -1,15 +1,27 @@
 package Runners;
 
+import com.aventstack.extentreports.service.ExtentService;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.AfterClass;
 
 @CucumberOptions (
-        features = "src/test/java/FutureFiles/_01_AutomatedMatchingTest.feature",
+        features = "src/test/java/FutureFiles/_02_TeamStudentMatchingTest.feature",
         glue = "StepDefinitions",
         plugin = "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
 )
 
 public class _01_Runner extends AbstractTestNGCucumberTests {
 
+    @AfterClass
+    public static void writeOnExtentReport(){
+        ExtentService.getInstance().setSystemInfo("Windows User Name",System.getProperty("user.name"));
+        ExtentService.getInstance().setSystemInfo("Time Zone",System.getProperty("user.timezone"));
+        ExtentService.getInstance().setSystemInfo("User Name","Tunahan Boyaci");
+        ExtentService.getInstance().setSystemInfo("Application Name","dhtmlgoodies");
+        ExtentService.getInstance().setSystemInfo("Operating System Info",System.getProperty("os.name"));
+        ExtentService.getInstance().setSystemInfo("Department","QA");
+        ExtentService.getInstance().setSystemInfo("Additional line","Additional info");
+    }
 
 }
